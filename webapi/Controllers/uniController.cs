82adf5176bb4a45c1357664006585eb82cd1206d
Shourav Kumar <shourav45@gmail.com>
2017-09-15 18:42:46 +0600

    first commit

---
+++ webapi/Controllers/uniController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace webapi.Controllers
+{
+    public class uniController : Controller
+    {
+        public class RestaurantApiController : ApiController
+        {
+            [System.Web.Http.HttpGet]
+            public HttpResponseMessage GetRestaurantInfo()
+            {
+
+                var result = new List<uniinfo>();
+                try
+                {
+                    RestaurantInformationHandler std = new RestaurantInformationHandler();
+                    List<RestaurantInformation> s = std.GetAll();
+                    if (s.Count > 0)
+                    {
+                        result = s;
+                        return Request.CreateResponse(HttpStatusCode.OK, result, "application/json");
+                    }
+                    else
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, result, "application/json");
+                    }
+                }
+                catch (Exception)
+                {
+
+                    return Request.CreateResponse(HttpStatusCode.OK, result, "application/json");
+                }
+            }
+
+        }
+    }
+}
